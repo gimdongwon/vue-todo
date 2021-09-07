90d4da76ae924c93a5a36bc221e9604bb03157cc
gimdongwon <ehddnjs8989@gmail.com>
2021-09-07 20:59:28 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="col-md-6 offset-md-3">
+      <h1 class="text-center mb-4">Todo Application with Vue</h1>
+      <input
+        type="text"
+        class="form-control mb-4"
+        v-model="userInput"
+        @keyup.enter="submit"
+      />
+      <button class="form-action" @click="submit">할일 추가</button>
+    </div>
+    <div class="todoList">
+      <ul class="list-group">
+        <template v-for="item in activeTodoList" v-bind:key="item">
+          <todo :label="item.label" @componentClick="toggleTodoState(item)" />
+        </template>
+      </ul>
+    </div>
+    <div class="text-right">
+      <button
+        class="btn btn-sm"
+        type="button"
+        @click="changeCurrentState('active')"
+      >
+        할일
+      </button>
+      <button
+        class="btn btn-sm"
+        type="button"
+        @click="changeCurrentState('done')"
+      >
+        완료
+      </button>
+      <button
+        class="btn btn-sm"
+        type="button"
+        @click="changeCurrentState('all')"
+      >
+        전체
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from "./components/Todo.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      userInput: "",
+      todoList: [],
+      currentState: "active",
+    };
+  },
+  computed: {
+    // class의 getter 함수처럼 동작함.
+    activeTodoList() {
+      return this.todoList.filter(
+        (todo) =>
+          this.currentState === "all" || todo.state === this.currentState
+      );
+    },
+  },
+  methods: {
+    changeCurrentState(state) {
+      this.currentState = state;
+    },
+    submit() {
+      this.todoList.push({
+        label: this.userInput,
+        state: "active",
+      });
+      this.userInput = "";
+    },
+    toggleTodoState(todo) {
+      if (todo.state === "active") {
+        todo.state = "done";
+      } else {
+        todo.state = "active";
+      }
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Todo,
+  },
+};
 </script>
 
 <style>
